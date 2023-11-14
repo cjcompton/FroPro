b1658b244b65a823dc915ad1e87a2719a519b292
charlieCompton <ccompton@lclark.edu>
2023-11-13 17:38:47 -0800

    got a better image, need better text, working on some basic icons

--- src/assets/HomeBody.jsx
+++ src/assets/HomeBody.jsx
@@ -3,31 +3,24 @@ import React from "react";
 //make a popup thatd be funny
 function HomeBody() {
     return(
-        /**
-        <section>
-            <div className="relative max-w-screen-2xl mx-auto">
+        <section className="h-auto">
+            <div className="max-w-screen-2xl mx-auto relative">
                 <img 
                     src="./double container healthy.png" 
                     alt="double container"
-                    className="w-full h-auto absolute z-10" 
+                    className="w-full h-auto relative z-10" 
                 />
-                // fix sizing
+                {/* fix the text sizing and positioning */}
                 <h1 className="text-red-700 z-20 max-w-md
                     text-sm top-4 left-4 
                     md:text-2xl md:top-10 md:left-10
                     lg:text-3xl lg:top-20 lg:left-20
                     xl:text-5xl 
-                    tracking-widest font-bold relative font-headFont">
+                    tracking-widest font-bold absolute font-headFont">
                     Achieve your goals, <br /> while feeling full
                 </h1>
             </div>
         </section>
-        */
-       <section>
-        <p className="text-center">
-            hello
-        </p>
-       </section>
     );
 }
 
--- src/assets/ReasonsNeeded.jsx
+++ src/assets/ReasonsNeeded.jsx
@@ -5,11 +5,21 @@ function ReasonsNeeded() {
         <section>
             <div className="relative z-20">
                 <div className="gap-4 md:columns-5 p-3 text-center">
-                    <p>5</p>
-                    <p>Reasons</p>
-                    <p>Needed</p>
-                    <p>To</p>
-                    <p>Buy</p>
+                    <div className="border-2 border-black rounded-md">
+                        <p>5</p>
+                    </div>
+                    <div className="border-2 border-black rounded-md">
+                        <p>Reasons</p>
+                    </div>
+                    <div className="border-2 border-black rounded-md">
+                        <p>Needed</p>
+                    </div>
+                    <div className="border-2 border-black rounded-md">
+                        <p>To</p>
+                    </div>
+                    <div className="border-2 border-black rounded-md">
+                        <p>Buy</p>
+                    </div>
                 </div>
             </div>
         </section>
